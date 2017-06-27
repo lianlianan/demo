c6f6b60d4ca072d80134b56102bcafd58711b375
Joseph Kim <josephjkim@gmail.com>
2017-06-26 17:17:53 -0700

    Removes geocoding

--- server/controllers/score.js
+++ server/controllers/score.js
@@ -28,26 +28,23 @@ exports.scoreAllHotels = (req, res, next) => {
   Promise.all(usHotels)
   .then(hotels => {
     hotels.forEach(hotel => {
-      let address = hotel.addressline1 + ',' + hotel.zipcode + ',' + 'US';
-      address.replace(/ /g,'+');
-      gmapOptions.path = '/maps/api/geocode/json' + '?address=' + encodeURI(address) + apiKey;
 
-      https.get(gmapOptions, (res) => {
+      rkOptions['path'] = '/api/score/cr/'
+        + hotel.latitude
+        + '/'
+        + hotel.longitude;
+
+      http.get(rkOptions, (res) => {
 
         const { statusCode } = res;
-        const contentType = res.headers['content-type'];
 
         let error;
         if(statusCode !== 200) {
           error = new Error('Request Failed.\n' +
                             `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-          error = new Error('Invalid content-type.\n' +
-                            `Expected application/json but received ${contentType}`);
         }
         if(error) {
           console.error(error.message);
-          // consume response data to free up memory
           res.resume();
           return;
         }
@@ -57,58 +54,12 @@ exports.scoreAllHotels = (req, res, next) => {
         res.on('data', (chunk) => { rawData += chunk; });
         res.on('end', () => {
           try {
-            const parsedData = JSON.parse(rawData);
+            const scoreData = JSON.parse(rawData);
 
-            if(parsedData && parsedData.results && parsedData.results.length > 0) {
-                rkOptions.path = '/api/score/cr/'
-                  + parsedData.results[0].geometry.location.lat
-                  + '/'
-                  + parsedData.results[0].geometry.location.lng;
+            hotel['score'] = scoreData.score;
+            console.log(hotel);
+            console.log(',');
 
-                http.get(rkOptions, (res) => {
-
-                  const { statusCode } = res;
-                  const contentType = res.headers['content-type'];
-
-                  let error;
-                  if(statusCode !== 200) {
-                    error = new Error('Request Failed.\n' +
-                                      `Status Code: ${statusCode}`);
-                  }
-                  if(error) {
-                    console.error(error.message);
-                    res.resume();
-                    return;
-                  }
-
-                  res.setEncoding('utf8');
-                  let rawData = '';
-                  res.on('data', (chunk) => { rawData += chunk; });
-                  res.on('end', () => {
-                    try {
-                      const scoreData = JSON.parse(rawData);
-                      const geometry = {
-                        location: {
-                          lat: parsedData.results[0].geometry.location.lat,
-                          lng: parsedData.results[0].geometry.location.lng
-                        }
-                      };
-
-                      hotel['geometry'] = geometry;
-                      hotel['score'] = scoreData.score;
-                      console.log(hotel + ',');
-
-                    } catch(e) {
-                      console.error(e.message);
-                    }
-                  });
-                }).on('error', (e) => {
-                  console.error(`Got error: ${e.message}`);
-                });
-              }
-              else {
-                console.log(hotel + ',');
-              }
           } catch(e) {
             console.error(e.message);
           }
@@ -116,6 +67,7 @@ exports.scoreAllHotels = (req, res, next) => {
       }).on('error', (e) => {
         console.error(`Got error: ${e.message}`);
       });
+      sleep(1000);
     });
   })
   .then(hotels => res.render('score/main') )
@@ -124,95 +76,9 @@ exports.scoreAllHotels = (req, res, next) => {
 
 
 
-  // Promise.all(ukHotels)
-  // .then(hotels => {
-  //   hotels.forEach(hotel => {
-  //     let address = hotel.addressline1 + ',' + hotel.zipcode + ',' + 'US';
-  //     address.replace(/ /g,'+');
-  //     gmapOptions.path = '/maps/api/geocode/json' + '?address=' + encodeURI(address) + apiKey;
-  //
-  //     https.get(gmapOptions, (res) => {
-  //
-  //       const { statusCode } = res;
-  //       const contentType = res.headers['content-type'];
-  //
-  //       let error;
-  //       if(statusCode !== 200) {
-  //         error = new Error('Request Failed.\n' +
-  //                           `Status Code: ${statusCode}`);
-  //       } else if (!/^application\/json/.test(contentType)) {
-  //         error = new Error('Invalid content-type.\n' +
-  //                           `Expected application/json but received ${contentType}`);
-  //       }
-  //       if(error) {
-  //         console.error(error.message);
-  //         // consume response data to free up memory
-  //         res.resume();
-  //         return;
-  //       }
-  //
-  //       res.setEncoding('utf8');
-  //       let rawData = '';
-  //       res.on('data', (chunk) => { rawData += chunk; });
-  //       res.on('end', () => {
-  //         try {
-  //           const parsedData = JSON.parse(rawData);
-  //
-  //             rkOptions.path = '/api/score/cr/'
-  //               + parsedData.results[0].geometry.location.lat
-  //               + '/'
-  //               + parsedData.results[0].geometry.location.lng;
-  //
-  //             http.get(rkOptions, (res) => {
-  //
-  //               const { statusCode } = res;
-  //               const contentType = res.headers['content-type'];
-  //
-  //               let error;
-  //               if(statusCode !== 200) {
-  //                 error = new Error('Request Failed.\n' +
-  //                                   `Status Code: ${statusCode}`);
-  //               }
-  //               if(error) {
-  //                 console.error(error.message);
-  //                 res.resume();
-  //                 return;
-  //               }
-  //
-  //               res.setEncoding('utf8');
-  //               let rawData = '';
-  //               res.on('data', (chunk) => { rawData += chunk; });
-  //               res.on('end', () => {
-  //                 try {
-  //                   const scoreData = JSON.parse(rawData);
-  //                   const geometry = {
-  //                     location: {
-  //                       lat: parsedData.results[0].geometry.location.lat,
-  //                       lng: parsedData.results[0].geometry.location.lng
-  //                     }
-  //                   };
-  //
-  //                   hotel['geometry'] = geometry;
-  //                   hotel['score'] = scoreData.score;
-  //                   console.log(hotel);
-  //
-  //                 } catch(e) {
-  //                   console.error(e.message);
-  //                 }
-  //               });
-  //             }).on('error', (e) => {
-  //               console.error(`Got error: ${e.message}`);
-  //             });
-  //
-  //         } catch(e) {
-  //           console.error(e.message);
-  //         }
-  //       });
-  //     }).on('error', (e) => {
-  //       console.error(`Got error: ${e.message}`);
-  //     });
-  //   });
-  // })
-  // .then(hotels => res.render('score/main') )
-  // .catch(e => next(e));
+
 };
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
